e46ed40f664bee91d4808d5514d33dd7f7eb25dd
joaovtfor <joaovtfor@hotmail.com>
2026-01-22 16:25:08 -0300

    ADD - Configurado stores unitárias do Redux para os testes

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,47 @@
+import { PropsWithChildren, ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
+import { configureStore, Store } from '@reduxjs/toolkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { theme } from '@/styles/theme';
+import { rootReducer, RootState } from '@/store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'wrapper'> {
+    preloadedState?: Partial<RootState>;
+    store?: Store;
+}
+
+const customRender = (
+    ui: ReactElement,
+    {
+        preloadedState = {},
+        store = configureStore({ reducer: rootReducer, preloadedState }),
+        ...renderOptions
+    }: ExtendedRenderOptions = {}
+) => {
+    const testQueryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: false,
+            },
+        },
+    });
+
+    function Wrapper({ children }: PropsWithChildren<{}>) {
+        return (
+            <Provider store={store}>
+                <QueryClientProvider client={testQueryClient}>
+                    <ThemeProvider theme={theme}>
+                        {children}
+                    </ThemeProvider>
+                </QueryClientProvider>
+            </Provider>
+        );
+    }
+
+    return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+};
+
+export * from '@testing-library/react';
+export { customRender as render };
