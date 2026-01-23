05e9fe61c5836d4b95091e98b38925a069b86861
joaovtfor <joaovtfor@hotmail.com>
2026-01-23 14:10:48 -0300

    FEAT - Modificado tipagem para a API

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface IProductAPI {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  price: string;
+  createdAt: string;
+}
+
+export interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  description?: string;
+}
+
+export interface ICartItem extends IProduct {
+  quantity: number;
+}
