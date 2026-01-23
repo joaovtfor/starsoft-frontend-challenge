37c0b5e3a1fedfe331d9cc0ef1433d6be3678911
joaovtfor <joaovtfor@hotmail.com>
2026-01-23 16:32:07 -0300

    FEAT - Adicionado Header e Footer ao core

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app';
 import { Provider as ReduxProvider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { store } from '@/store';
-import { GlobalStyles } from '@/styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
+
+import { store } from '@/store';
 import { theme } from '@/styles/theme';
+import { GlobalStyles } from '@/styles/GlobalStyles';
+
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,7 +25,22 @@ export default function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <GlobalStyles />
-          <Component {...pageProps} />
+
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              minHeight: '100vh',
+            }}
+          >
+            <Header />
+
+            <div style={{ flex: 1 }}>
+              <Component {...pageProps} />
+            </div>
+
+            <Footer />
+          </div>
         </ThemeProvider>
       </QueryClientProvider>
     </ReduxProvider>
